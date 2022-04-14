810301fc1445b4f1103b06a97a8cd29a11b3767b
hungryjayy <juwonson2068@naver.com>
2022-04-14 22:28:35 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/imt/ImtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.imt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImtApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/imt/ImtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.imt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImtApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

4af01e9c1e9aa76a7a3cb2a062875691f64e0288
HungryJayy <49271821+hungryjayy@users.noreply.github.com>
2022-04-17 20:55:55 +0900

    Feature: swagger UI (#3)
    * docs: Add swagger UI
    
    * docs: Add readme
    
    * chore: Modify directory

---
+++ README.md
@@ -0,0 +1,18 @@
+## IMT server
+
+<br>
+
+### How to run
+
+1. `./scripts/build.sh`: 빌드 쉘 스크립트를 실행해 서버를 빌드하고 `jar`파일을 생성합니다. `jar` 파일은 `./build/libs`에 생성됩니다.
+2. `./scripts/start.sh`: 서버 실행 쉘 스크립트 실행합니다. 서버는 백그라운드로 실행됩니다.
+3. `.scripts/stop.sh`: 백그라운드로 실행중인 서버를 종료하기 위해서 직접 프로세스를 종료하거나 서버 종료 스크립트를 실행합니다.
+
+<br>
+
+### API docs
+
+`{URL}/swagger-ui/index.html` 을 통해 API 문서를 확인 가능합니다.
+
+e.g. http://3.39.137.243:8080/swagger-ui-index.html
+
--- src/main/kotlin/com/example/imt/game/presentation/GameController.kt
+++ src/main/kotlin/com/example/imt/game/presentation/GameController.kt
@@ -4,23 +4,33 @@ import com.example.imt.celebrity.domain.Gender
 import com.example.imt.common.API_URI
 import com.example.imt.game.application.GameService
 import com.example.imt.game.presentation.GameController.Companion.GAME_URI
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.annotations.ApiIgnore
 
 @RestController
 @RequestMapping(GAME_URI)
 class GameController(
     private val gameService: GameService,
 ) {
-
+    @Operation(summary = "테스트 시작 API", description = "성별에 따른 랜덤 이미지 파일명 6개 반환")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "OK"),
+        ApiResponse(responseCode = "400", description = "Bad Request"),
+        ApiResponse(responseCode = "500", description = "Internal Server Error")
+    )
     @GetMapping(value = ["/initial-images"], params = ["gender"])
     @ResponseStatus(value = HttpStatus.OK)
     fun start(@RequestParam gender: Gender) = gameService.getStartResult(gender)
 
+    @ApiIgnore
     @GetMapping(params = ["stage", "gender"])
     @ResponseStatus(value = HttpStatus.OK)
     fun getStageResult(
@@ -30,6 +40,7 @@ class GameController(
         TODO()
     }
 
+    @ApiIgnore
     @GetMapping(value = ["/results"], params = ["gender"])
     @ResponseStatus(value = HttpStatus.OK)
     fun getGameResult(
